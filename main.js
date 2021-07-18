28532a7f226acf647e844fe995c43d39923a66e2
Valodzina <ulyanavalodzina@gmail.com>
2021-07-18 19:19:59 +0300

    burger

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+$(function() {
+    $('.rev_slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed:4000,
+        arrows:false,
+        
+    })
+})
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+$(document).ready(function() {
+    $('.header_burger').click(function(event){
+        $('.header_burger,.header_menu').toggleClass('active');
+        $('.body').toggleClass('lock');
+    });
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+});
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
 
