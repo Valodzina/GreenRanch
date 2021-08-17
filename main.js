a1a2a354b7cd607b172a14b62a1b41b746a171de
Valodzina <ulyanavalodzina@gmail.com>
2021-08-17 09:57:01 +0300

    photo

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ $(function() {
         infinite: false,
         speed: 300,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         responsive: [
             {
                 breakpoint: 1024,
